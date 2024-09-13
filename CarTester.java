6483d8ebe32c658a5d28d6a4d0ee1d5502e6c4ef
paulomiim <paulomimehta0208@yahoo.com>
2024-09-13 00:20:01 -0700

    Update CarTester.java
    car tester class completed.

--- CarTester.java
+++ CarTester.java
@@ -1,1 +1,38 @@
+import java.util.Scanner;
+public class CarTester{
+    
+    public static Car addCar(){
+        Scanner myScanner = new Scanner(System.in); //make scanner
+        
+        System.out.println("Please enter car name: ");
+        String carName = myScanner.nextLine();
+        
+        System.out.println("Please enter cars miles: ");
+        Double milesDriven = myScanner.nextDouble();
+        
+        System.out.println("Please enter cars gallons: ");
+        Double gallonsUsed = myScanner.nextDouble();
+        
+        Car myCar1 = new Car(carName, milesDriven, gallonsUsed);
+        return myCar1;
+        
+        
+    }
+    
+    
+    
+    public static void main(String[] args){
+        Car car1 = addCar();
+        System.out.println(car1.toString());
+        
+        Car car2 = addCar();
+        System.out.println(car2.toString());
+        
+        
+        
+        
+        
+        
+    }
+}
 
